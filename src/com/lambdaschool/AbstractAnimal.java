53a2c079f758eeacfbaf80fb8eaeeb85335229f2
Kevin Tena <kevten09@gmail.com>
2019-02-06 16:15:51 -0600

    Implemented Animal Abstract class

--- src/com/lambdaschool/AbstractAnimal.java
+++ src/com/lambdaschool/AbstractAnimal.java
@@ -1,4 +1,28 @@
 package com.lambdaschool;
 
 public abstract class AbstractAnimal {
+    int maxId = 0;
+    int id;
+    String name;
+    int yearDiscovered;
+
+    public AbstractAnimal(String name, int yearDiscovered) {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public abstract String move();
+    public abstract String breath();
+    public abstract String reproduce();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearDiscovered() {
+        return yearDiscovered;
+    }
 }
